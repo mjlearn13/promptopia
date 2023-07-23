07f743095831f3d47f3663ad0d1d47af1b3ded5a
mjlearn13 <80919195+mjlearn13@users.noreply.github.com>
2023-07-23 15:01:27 +0100

    initial project files

---
+++ app/layout.jsx
@@ -0,0 +1,13 @@
+import '@styles/globals.css';
+
+export const metadata = {
+    titele: "Promptopia",
+    description: 'Discover & Share AI Prompts'
+}
+
+const layout = () => {
+  return (
+    <div>layout</div>
+  )
+}
+export default layout
